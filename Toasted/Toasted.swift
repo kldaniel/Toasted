3e280e754450a2ae7ec9e834307bd7b03916dca3
Daniel Kiss <lszdkdev@gmail.com>
2018-08-19 22:53:44 +0200

    Copyright fix

--- Toasted/Toasted.swift
+++ Toasted/Toasted.swift
@@ -3,7 +3,7 @@
 //  Toasted
 //
 //  Created by Daniel Kiss on 2018. 08. 09..
-//  Copyright © 2018. Toasted. All rights reserved.
+//  Copyright © 2018. Daniel Kiss. All rights reserved.
 //
 
 import Foundation
