b9b26bf0d8d7b172928b3ecfc7353395fe9dfbec
Daniel Kiss <lszdkdev@gmail.com>
2018-08-23 21:59:19 +0200

    Modify basic example to be able to show 2 different toast.

--- Demo/Demo/Tutorials/TutorialBasicController.swift
+++ Demo/Demo/Tutorials/TutorialBasicController.swift
@@ -12,11 +12,11 @@ import Toasted
 class TutorialBasicController: UIViewController {
 
     @IBOutlet weak var show1Button: UIButton!
+    @IBOutlet weak var show2Button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Basic Demo"
-
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,6 +31,13 @@ extension TutorialBasicController {
     @IBAction func actionShow1(_ sender: UIButton) {
 
         let toast: Toast = Toast(title: "Hello, Toasted! This is a simple toast you can display on the screen. It is useful for showing quick information to the user.")
+        toast.show(in: self.view)
+
+    }
+
+    @IBAction func actionShow2(_ sender: UIButton) {
+
+        let toast: Toast = Toast(title: "Another quick message.")
         toast.show(in: self.view)
 
     }
